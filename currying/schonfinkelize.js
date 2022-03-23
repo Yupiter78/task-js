12bfddeb5434bc790451d8371945295124821091
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-03-23 23:21:35 +0300

    feat: add currying task

--- currying/schonfinkelize.js
+++ currying/schonfinkelize.js
@@ -1,28 +1,37 @@
 const _sum3 = (x, y, z) => x + y + z;
 
-function schonfinkelize(fn) {
-    const slice = Array.prototype.slice;
-    console.log("slice:", slice);
-    const storedArgs = slice.call(arguments, 1);
-    console.log("storedArgs:", storedArgs);
-    return function () {
-        const newArgs = slice.call(arguments);
-        const args = storedArgs.concat(newArgs);
+// function schonfinkelize(fn) {
+//     const slice = Array.prototype.slice;
+//     console.log("slice:", slice);
+//     const storedArgs = slice.call(arguments, 1);
+//     console.log("storedArgs:", storedArgs);
+//     return function () {
+//         const newArgs = slice.call(arguments);
+//         const args = storedArgs.concat(newArgs);
+//         return fn.apply(null, args);
+//     };
+// }
+
+function schonfinkelize(fn, ...rest) {
+    return function (...newRest) {
+        const args = [...rest, ...newRest];
+        console.log("args:", args);
         return fn.apply(null, args);
     };
 }
 
 // обычная функция
 function add(x, y) {
+    console.log("x, y:", x, y);
     return x + y;
 }
 
 // каррировать существующую функцию и получить новую
-// const newAdd = schonfinkelize(add, 5);
-// console.log(newAdd(4)); // 9
+const newAdd = schonfinkelize(add, 5);
+console.log(newAdd(4)); // 9
 // другой вариант – вызвать новую функцию сразу же
-// console.log(schonfinkelize(add, 6)(7)); // 13
-// console.log(schonfinkelize(_sum3, 6, 6)(7)); //19
+console.log(schonfinkelize(add, 6)(7)); // 13
+console.log(schonfinkelize(_sum3, 6, 6)(7)); //19
 console.log(schonfinkelize(_sum3, 1, 3)(2)); //6
 
 
@@ -65,3 +74,35 @@ const sum3 = curry(_sum3);
 console.log(sum3(1)(3)(2)); // 6
 /*
 console.log(sum4(1)(3)(2)(4)); // 10*/
+
+function sum(a) {
+    console.log("a:", a);
+
+    let currentSum = a;
+
+    function f(b) {
+        console.log("b:", b);
+        currentSum += b;
+        return f;
+    }
+
+    f.toString = function () {
+        return currentSum;
+    };
+
+    return f;
+}
+
+console.log("sum:",sum(1)(2).toString()); // 3
+alert(sum(5)(-1)(2)); // 6
+console.log(sum(6)(-1)(-2)(-3)); // 0
+console.log(sum(0)(1)(2)(3)(4)(5)); // 15
+
+console.log(+sum(1)(2) === 3); // 1 + 2
+console.log(+sum(1)(2)(3) === 6); // 1 + 2 + 3
+console.log(+sum(5)(-1)(2) === 6)
+console.log(+sum(6)(-1)(-2)(-3) === 0)
+console.log(+sum(0)(1)(2)(3)(4)(5) === 15)
+
+console.log("{}:", {});
+alert({});

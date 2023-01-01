50e1d81399d7edccd0301056778967a582a72875
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-01-01 22:29:05 +0300

    feat: add task object keys

--- object_keys/object_keys.js
+++ object_keys/object_keys.js
@@ -5,10 +5,10 @@ function merge(target, ...sources) {
         for (let key of Object.keys(source)) {
             if (!(key in target)) { // Это отличается от Object .assign ()
                 target[key] = source[key];
-                return target;
             }
         }
     }
+    return target;
 }
-console.log(Object.assign ({x : 1}, {x: 2, y: 2}, {y: 3, z: 4})); // => {x: 2, y: 3, z: 4}
-console.log(merge({x: 1}, {x: 2, y: 2}, {y: 3, z: 4})); // => {x: 1, y: 2, z: 4}
+console.log(Object.assign ({x : 1}, {x: 2, y: 2}, {y: 3, z: 4}), "{x: 2, y: 3, z: 4}");
+console.log(merge({x: 1}, {x: 2, y: 2}, {y: 3, z: 4}), "{x: 1, y: 2, z: 4}");

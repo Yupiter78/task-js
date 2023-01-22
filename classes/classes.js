a0fb91ed9f7d0689f32131c40e7b25c0373e60b5
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-01-22 23:35:13 +0300

    feat: add task classes

--- classes/classes.js
+++ classes/classes.js
@@ -1,3 +1,5 @@
+//9.1 Classes and Prototypes
+
 // Example 9-1. A simple JavaScript class
 // This is a factory function that returns a new range object.
 function range(from, to) {
@@ -25,11 +27,43 @@ function range(from, to) {
             yield x;
     },
 // Return a string representation of the range
-    toString() { return "(" + this.from + "..." + this.to +
-        ")"; }
+    toString() { return "(" + this.from + "..." + this.to + ")"; }
 };
 // Here are example uses of a range object.
 let r = range(1,3); // Create a range object
 console.log(r.includes(2), "=> true") // => true: 2 is in the range
 console.log(r.toString(), "=> (1...3)"); // => "(1...3)"
 console.log([...r], " => [1, 2, 3]") // => [1, 2, 3]; convert to an array via iterator
+
+
+// Classes and Constructors
+
+// Example 9-2. A Range class using a constructor
+// This is a constructor function that initializes new Range objects.
+// Note that it does not create or return the object. It just initializes this.
+    function Range(from, to) {
+// Store the start and end points (state) of this new range object.
+// These are non inherited properties that are unique to this object.
+    this.from = from;
+    this.to = to;
+}
+// All Range objects inherit from this object.
+// Note that the property name must be "prototype" for this to work.
+    Range.prototype = {
+// Return true if x is in the range, false otherwise
+// This method works for textual and Date ranges as well as numeric.
+    includes: function(x) { return this.from <= x && x <= this.to; },
+// A generator function that makes instances of the class iterable.
+// Note that it only works for numeric ranges.
+    [Symbol.iterator]: function*() {
+        for(let x = Math.ceil(this.from); x <= this.to; x++)
+            yield x;
+    },
+// Return a string representation of the range
+    toString: function() { return "(" + this.from + "..." + this.to + ")"; }
+};
+// Here are example uses of this new Range class
+let r_2 = new Range(1,3); // Create a Range object; note the use of new
+console.log(r_2.includes(2), "=> true") // => true: 2 is in the range
+console.log(r_2.toString(), "=> (1...3)"); // => "(1...3)"
+console.log([...r_2], "=> [1, 2, 3]") // => [1, 2, 3]; convert to an array via iterator

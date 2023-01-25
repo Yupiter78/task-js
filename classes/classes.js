d37fe28243e6cf9a896df9b10fc68647fae05c63
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-01-25 23:50:44 +0300

    feat: add task classes

--- classes/classes.js
+++ classes/classes.js
@@ -76,8 +76,8 @@ console.log([...r_2], "=> [1, 2, 3]") // => [1, 2, 3]; convert to an array via i
 
 let F = function() {}; // This is a function object.
 let p = F.prototype; // This is the prototype object associated with F.
-let c = p.constructor; // This is the function associated with the prototype.
-console.log("c === F:", c === F) // => true: F.prototype.constructor === F for any F
+let constr = p.constructor; // This is the function associated with the prototype.
+console.log("constr === F:", constr === F) // => true: F.prototype.constructor === F for any F
 
 let o = new F(); // Create an object o of class F
 console.log("o.constructor === F:", o.constructor === F) // => true: the constructor property specifies the class
@@ -110,3 +110,97 @@ let r_3 = new newRange(1,3); // Create a Range object
 console.log(r_3.includes(2), "=> true") // => true: 2 is in the range
 console.log(r_3.toString(), "=> (1...3)"); // => "(1...3)"
 console.log([...r_3], "=> [1, 2, 3]") // => [1, 2, 3]; convert to an array via iterator
+
+
+function sum(a, b) {
+    return a + b;
+}
+
+for (let key in sum) {
+    console.log(key, ":", sum[key]);
+}
+sum.test = "TEST";
+
+console.log("SUM:", Object.getOwnPropertyNames(sum));
+
+const keys = Reflect.ownKeys(sum)
+console.log("SUM_ keys:", keys);
+
+const sumObj = {};
+for (let prop of keys) {
+    sumObj[prop] = Reflect.get(sum, prop);
+}
+console.log("sumObj:", sumObj);
+console.log("sum:", sum);
+console.log("sum.test:", sum.test);
+console.dir("sum:", sum);
+console.log("sum.prototype:", sum.prototype);
+
+// Example 9-4. Complex.js: a complex number class:
+/**
+ * Instances of this Complex class represent complex numbers.
+ * Recall that a complex number is the sum of a real number and
+ an
+ * imaginary number and that the imaginary number i is the
+ square root of -1.
+ */
+class Complex {
+// Once class field declarations are standardized, we could declare
+// private fields to hold the real and imaginary parts of a complex number
+// here, with code like this:
+//
+// #r = 0;
+// #i = 0;
+// This constructor function defines the instance fields r and i on every
+// instance it creates. These fields hold the real and imaginary parts of
+// the complex number: they are the state of the object.
+    constructor(real, imaginary) {
+        this.r = real; // This field holds the real part of the number.
+        this.i = imaginary; // This field holds the imaginary part.
+    }
+// Here are two instance methods for addition and multiplication
+// of complex numbers. If c and d are instances of this class, we
+// might write c.plus(d) or d.times(c)
+    plus(that) {
+        return new Complex(this.r + that.r, this.i + that.i);
+    }
+    times(that) {
+        return new Complex(this.r * that.r - this.i * that.i,
+            this.r * that.i + this.i * that.r);
+    }
+// And here are static variants of the complex arithmetic methods.
+// We could write Complex.sum(c,d) and Complex.product(c,d)
+    static sum(c, d) { return c.plus(d); }
+    static product(c, d) { return c.times(d); }
+// These are some instance methods that are defined as getters,
+// so they're used like fields. The real and imaginary getters would
+// be useful if we were using private fields this.#r and this.#i
+    get real() { return this.r; }
+    get imaginary() { return this.i; }
+    get magnitude() { return Math.hypot(this.r, this.i); }
+// Classes should almost always have a toString() method
+    toString() { return `{${this.r},${this.i}}`; }
+// It is often useful to define a method for testing whether
+// two instances of your class represent the same value
+    equals(that) {
+        return that instanceof Complex &&
+            this.r === that.r &&
+            this.i === that.i;
+    }
+// Once static fields are supported inside class bodies, we could
+// define a useful Complex.ZERO constant like this:
+// static ZERO = new Complex(0,0);
+}
+// Here are some class fields that hold useful predefined complex numbers.
+Complex.ZERO = new Complex(0,0);
+Complex.ONE = new Complex(1,0);
+Complex.I = new Complex(0,1);
+
+let c = new Complex(2, 3); // Create a new object with the constructor
+console.log("c:", c);
+let d = new Complex(c.i, c.r); // Use instance fields of c
+console.log("d:", d);
+console.log("c.plus(d).toString():", c.plus(d).toString(), "=> {5,5}"); // => "{5,5}"; use instance methods
+console.log("c.magnitude:", c.magnitude, "Math.hypot(2,3)"); // => Math.hypot(2,3); use a getter function
+console.log("Complex.product(c, d):", Complex.product(c, d), "new Complex(0, 13)"); // => new Complex(0, 13); a static method
+console.log("Complex.ZERO.toString():", Complex.ZERO.toString(), "=> {0,0}") // => "{0,0}"; a static property

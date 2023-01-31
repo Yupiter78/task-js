57fa36f238ef337d7f61e6d48d618dcc9eafc986
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-01-31 23:09:10 +0300

    feat: add task classes

--- classes/classes.js
+++ classes/classes.js
@@ -571,3 +571,68 @@ class Rabbit_2 extends Animal_5 {
 
 let rabbit_5 = new Rabbit_2("Белый кролик"); // Белый кролик
 console.log(rabbit_5.name);
+
+
+class Clock_3 {
+    constructor({ template }) {
+        this.template = template;
+    }
+
+    render() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
+    }
+}
+
+let clock_3 = new Clock_3({
+    template: 'h:m:s'
+});
+//clock_3.start();
+
+class ExtendedClock extends Clock_3{
+    precision = 1000;
+    constructor({template, precision}) {
+        super({template});
+        this.precision = precision;
+    }
+    render() {
+        super.render()
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), this.precision);
+    }
+}
+
+
+  let lowResolutionClock = new ExtendedClock({
+    template: 'h:m:s',
+    precision: 10000
+  });
+
+  lowResolutionClock.start();

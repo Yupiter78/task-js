fb5ea3a49bfc1c9187b60ea0cf4d6fde239ee29b
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-02-01 23:45:36 +0300

    feat: add task classes

--- classes/classes.js
+++ classes/classes.js
@@ -685,3 +685,28 @@ let rabbit_6 = new Rabbit_4("Кроль");
 
 console.log( rabbit_6.hasOwnProperty('name') ); // true
 console.log( Rabbit_4.getOwnPropertyNames({a: 1, b: 2}) ); // a,b
+
+
+class CoffeeMachine {
+    _waterAmount = 0;
+
+    set waterAmount(value) {
+        if (value < 0) throw new Error("Отрицательное количество воды");
+        this._waterAmount = value;
+    }
+
+    get waterAmount() {
+        return this._waterAmount;
+    }
+
+    constructor(power) {
+        this._power = power;
+    }
+
+}
+
+// создаём новую кофеварку
+let coffeeMachine = new CoffeeMachine(100);
+
+// устанавливаем количество воды
+coffeeMachine.waterAmount = -10; // Error: Отрицательное количество воды

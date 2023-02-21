8d9f7cae4d40ff85d502657e1c949da8cf8515d5
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-02-21 23:37:38 +0300

    feat: add task classes

--- classes/mixins.js
+++ classes/mixins.js
@@ -23,3 +23,37 @@ new User("Dude").sayHi(); // Hello Dude!
 
 // There’s no inheritance, but a simple method copying.
 // So User may inherit from another class and also include the mixin to “mix-in” the additional methods
+
+// Mixins can make use of inheritance inside themselves.
+
+// For instance, here sayHiMixin inherits from sayMixin:
+
+    let sayMixin = {
+        say(phrase) {
+            console.log(phrase);
+        }
+    };
+
+let sayHiMixin_2 = {
+    __proto__: sayMixin, // (or we could use Object.setPrototypeOf to set the prototype here)
+
+    sayHi() {
+        // call parent method
+        super.say(`Hello ${this.name}`); // (*)
+    },
+    sayBye() {
+        super.say(`Bye ${this.name}`); // (*)
+    }
+};
+
+class User_2 {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+// copy the methods
+Object.assign(User_2.prototype, sayHiMixin_2);
+
+// now User_2 can say hi
+new User_2("Dude").sayHi(); // Hello Dude!

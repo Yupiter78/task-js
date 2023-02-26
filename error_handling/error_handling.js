fac0230d03db3866192efa484b6b25b5c7199bd7
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-02-26 22:00:02 +0300

    feat: add task error handling

--- error_handling/error_handling.js
+++ error_handling/error_handling.js
@@ -40,3 +40,31 @@ try {
 } catch (err) {
     log( "JSON Error: " + err.message ); // JSON Error: Incomplete data: no name
 }
+
+
+function readData() {
+    let json = '{ "age": 30 }';
+
+    try {
+        // let user = JSON.parse(json);
+        //
+        // if (!user.name) {
+        //     throw new SyntaxError("Данные неполны: нет имени");
+        // }
+
+        blabla(); // неожиданная ошибка
+
+        alert( user.name );
+    } catch (err) {
+        // ...
+        if (!(err instanceof SyntaxError)) {
+            throw err; // rethrow (don't know how to deal with it)
+        }
+    }
+}
+
+try {
+    readData();
+} catch (err) {
+    log( "External catch got: " + err ); // caught it!
+}

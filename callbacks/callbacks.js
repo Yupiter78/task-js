160e22b801b6f784e67cd615f4b0aea242e366c2
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-03-05 23:45:55 +0300

    feat: add task callbacks

---
+++ callbacks/callbacks.js
@@ -0,0 +1,13 @@
+
+const log = console.log;
+function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = () => callback(script);
+    document.head.append(script);
+}
+
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+    log(`Cool, the script ${script.src} is loaded`);
+    log( _ ); // _ is a function declared in the loaded script
+});

3ebff82cc11b835c0b9d5146a1aa4637b63c28fa
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-03-12 23:58:17 +0300

    feat: add task css-animations

--- css-animations/css-animations.js
+++ css-animations/css-animations.js
@@ -2,19 +2,24 @@ const log = console.log;
 
 const color = document.getElementById("color");
 log("color:", color);
+log("window.getComputedStyle:", window.getComputedStyle(color));
+log("color.style:", color.style);
 log("color.style.backgroundColor:", color.style.backgroundColor);
+log("color.style.fontSize:", color.style.font);
 
 changeColor = () => {
-    if (color.style.backgroundColor === "red" || color.style.backgroundColor === "") {
-        return "blue";
-    } else if (color.style.backgroundColor === "blue") {
+    if (color.style.backgroundColor === "blue" || color.style.backgroundColor === "") {
         return "red";
+    } else if (color.style.backgroundColor === "red") {
+        return "blue";
     }
 };
 
 color.onclick = function() {
     log("CLICK");
     log("color.style.backgroundColor:", color.style.backgroundColor);
+    log("color.style.fontSize:", color.style.fontSize);
     this.style.backgroundColor = changeColor();
+    this.style.fontSize = "36px";
     log("changeColor():", changeColor());
 };

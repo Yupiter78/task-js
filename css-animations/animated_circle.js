197674d690066d96f26b4abe5be291bac2e100f9
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-03-18 00:42:26 +0300

    feat: add task css-animations

--- css-animations/animated_circle.js
+++ css-animations/animated_circle.js
@@ -1,28 +1,21 @@
 
-let div = document.querySelector('div');
-let computedStyle = getComputedStyle(div);
-console.log("computedStyle.width:", computedStyle.width);
+// let div = document.querySelector('div');
+//
+// function showCircle() {
+//     const computedStyle = getComputedStyle(div),
+//     radius = computedStyle.width === "20px" ? 250 : 10;
+//     div.style.width = radius * 2 + 'px';
+//     div.style.height = radius * 2 + 'px';
+// }
 
+let div = document.querySelector('div'),
+    isLarge = false;
 
-function showCircle(radius) {
-    setTimeout(() => {
-        div.style.width = radius * 2 + 'px';
-        div.style.height = radius * 2 + 'px';
-    }, 0);
-     computedStyle = getComputedStyle(div);
-    console.log("computedStyle.width:", computedStyle.width);
+function showCircle() {
+    let radius = isLarge ? 10 : 250;
+    div.style.width = radius * 2 + 'px';
+    div.style.height = radius * 2 + 'px';
+    isLarge = !isLarge;
 }
 
-const handler_1 = () => {
-    console.log("HANDLER_1");
-    showCircle(250);
-}
-const handler_2 = () => {
-    console.log("HANDLER_2");
-    showCircle(10);
-}
-
-
-const handler = computedStyle.width === "20px" ? handler_1 : handler_2;
-
-div.addEventListener("click", handler);
+div.addEventListener("click", showCircle);

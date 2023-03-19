773ac55a8edfe79921c134f8aa9cc3daa625dcd3
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-03-20 00:12:15 +0300

    feat: add task css-animations

--- css-animations/transition.js
+++ css-animations/transition.js
@@ -1,4 +1,6 @@
 const stripe = document.querySelector("#stripe");
 console.log("stripe:", stripe);
 
-stripe.addEventListener("click", () => stripe.classList.add("animate"));
+// stripe.addEventListener("click", () => stripe.classList.add("animate"));
+
+stripe.onclick = () => stripe.classList.add("animate");

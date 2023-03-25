0b20f613b5618cef4ea5152cf2dc6e3c322c6f85
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-03-26 00:19:44 +0300

    feat: add task css-animations

--- css-animations/animate_a_plane.js
+++ css-animations/animate_a_plane.js
@@ -10,12 +10,15 @@ console.log(plane);
 
 const computedStyle = getComputedStyle(plane);
 console.log("computedStyle.width:", computedStyle.width);
-// const changeSize =  (width, height) => {
-//     this.style.width = width * 10 + "px";
-//     this.style.height = height * 10 + "px";
-// }
 
-plane.addEventListener("click", function () {
-    this.style.width = computedStyle.width * 10 + "px";
-    this.style.height = computedStyle.height * 10 + "px";
-});
+
+const handleChangeSize = function () {
+    const changeSize = (width, height) => {
+        this.style.width = parseInt(width) * 10 + "px";
+        this.style.height = parseInt(height) * 10 + "px";
+    }
+    changeSize(computedStyle.width, computedStyle.height);
+    this.removeEventListener("click", handleChangeSize);
+}
+
+plane.addEventListener("click", handleChangeSize);

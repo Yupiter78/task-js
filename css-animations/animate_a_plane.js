785011481722bc5946c119369450f5406a7e716c
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-03-25 23:58:31 +0300

    feat: add task css-animations

--- css-animations/animate_a_plane.js
+++ css-animations/animate_a_plane.js
@@ -1,9 +1,21 @@
 const plane = document.querySelector("#flyJet");
 console.log(plane);
 
-plane.onclick = function () {
-    this.onclick = null;
+// plane.onclick = function () {
+//     this.onclick = null;
+//
+//     this.style.width = "400px";
+//     this.style.height = "240px";
+// }
 
-    this.style.width = "400px";
-    this.style.height = "240px";
-}
+const computedStyle = getComputedStyle(plane);
+console.log("computedStyle.width:", computedStyle.width);
+// const changeSize =  (width, height) => {
+//     this.style.width = width * 10 + "px";
+//     this.style.height = height * 10 + "px";
+// }
+
+plane.addEventListener("click", function () {
+    this.style.width = computedStyle.width * 10 + "px";
+    this.style.height = computedStyle.height * 10 + "px";
+});

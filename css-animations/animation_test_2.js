fe94bc41efccfc83816779f8bd9feb437cd9c6d9
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-03-29 00:21:45 +0300

    feat: add task css-animations

--- css-animations/animation_test_2.js
+++ css-animations/animation_test_2.js
@@ -1,14 +1,21 @@
 function showCircle(cx, cy, radius) {
-    let div = document.createElement('div');
-    div.style.width = 0;
-    div.style.height = 0;
-    div.style.left = cx + 'px';
-    div.style.top = cy + 'px';
-    div.className = 'circle';
+    const div = document.createElement("div");
+    div.classList.add("circle");
+    div.style.width = "0px";
+    div.style.height = "0px";
     document.body.append(div);
 
+    div.style.left = cx + "px";
+    div.style.top = cy + "px";
+
     setTimeout(() => {
-        div.style.width = radius * 2 + 'px';
-        div.style.height = radius * 2 + 'px';
+        div.style.width = radius * 2 + "px";
+        div.style.height = radius * 2 + "px";
     }, 0);
+
 }
+
+const button = document.createElement("button");
+button.textContent = "showCircle(150, 150, 100)"
+document.body.append(button);
+button.onclick = () => showCircle(150, 150, 100);

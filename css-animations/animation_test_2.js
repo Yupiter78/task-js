ca54d6c422777cb6bd92162ec5fb0fa127396316
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-03-30 22:51:50 +0300

    feat: add task css-animations

--- css-animations/animation_test_2.js
+++ css-animations/animation_test_2.js
@@ -1,4 +1,4 @@
-function showCircle(cx, cy, radius) {
+function showCircle(cx, cy, radius, callback) {
     const div = document.createElement("div");
     div.style.width = "0px";
     div.style.height = "0px";
@@ -11,10 +11,14 @@ function showCircle(cx, cy, radius) {
         div.style.width = radius * 2 + "px";
         div.style.height = radius * 2 + "px";
     }, 0);
+    setTimeout(() => callback(div), 2000);
 
 }
 
 const button = document.createElement("button");
 button.textContent = "showCircle(150, 150, 100)"
 document.body.append(button);
-button.onclick = () => showCircle(150, 150, 100);
+button.onclick = () => showCircle(150, 150, 100, div => {
+    div.classList.add('message-ball');
+    div.append("Hello, world!");
+});

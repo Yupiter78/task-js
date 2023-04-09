b396f8320baa68cb426f298d55491d8ebe546dff
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-04-10 00:54:15 +0300

    feat: add task promise

--- promise/countries.js
+++ promise/countries.js
@@ -1,18 +1,17 @@
-const list = document.createElement("ol");
+
 
 fetch('https://restcountries.com/v3.1/all')
     .then(response => response.json())
     .then(countries => {
-        const countryList = countries.map(country => {
-            return `
-            <li>
-            <p>${country.name?.common}</p>
-            <img src=${country.flags.png} alt=${country.flags.alt} />
-            </li>`;
-        });
-
-        list.innerHTML = countryList.join("");
-        document.body.append(list);
+        const template = countries.map(country => (
+            `<li>
+                <p>${country.name?.common}</p>
+                <img src=${country.flags.png} alt=${country.flags.alt} />
+            </li>`));
+        document.body.innerHTML = `
+        <ol>
+            ${template.join("")}
+        </ol>`;
     })
-    .catch(err => console.log(err))
-    .finally()
+    .catch(({name, message}) => console.log(name, message))
+    .finally();

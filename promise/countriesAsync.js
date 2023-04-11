e4595bde70521785a47b7c9971f6a1a6a338cd9a
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-04-11 23:12:30 +0300

    feat: add task promise

---
+++ promise/countriesAsync.js
@@ -0,0 +1,21 @@
+async function getCountries() {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        ((countries) => {
+            const template = countries.map(country => (
+                `<li>
+                    <p>${country.name?.common}</p>
+                    <img src=${country.flags.png} alt=${country.flags.alt} />
+                </li>`));
+            document.body.innerHTML = `
+            <ol>
+                ${template.join("")}
+            </ol>`;
+        })(await response.json());
+    } catch ({name, message}){
+        console.log(name, message);
+    } finally {
+        console.log("finally")
+    }
+}
+getCountries();

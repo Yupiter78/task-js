5145755b35be4b659d6dff57c6449972639832e6
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-05 22:50:34 +0300

    feat: add task make a spiral

--- make_a_spiral/make_a_spiral.js
+++ make_a_spiral/make_a_spiral.js
@@ -1,18 +1,18 @@
 function makeASpiral(n) {
-    const a = new Array(n * n).fill(null);
-    const dirs = [1, n, -1, -n];
-    const result = [];
-    let dir = 0;
-    let addr = 0;
+    const a = new Array(n * n).fill(null),
+        dirs = [1, n, -1, -n],
+        result = [];
+    let dir = 0,
+        addr = 0;
 
     for (let i = 1; i <= n * n; i++) {
         a[addr] = i;
 
-        if (addr === n - 1 || addr === n * n - 1 || addr === n * n - n || a[addr + dirs[dir]] !== null) {
+        if (addr === n - 1 || addr === n * n - 1 || a[addr + dirs[dir]] !== null) {
             dir = (dir + 1) % 4;
         }
-
         addr += dirs[dir];
+
     }
     
     for (let y = 0; y < n; y++) {
@@ -21,5 +21,5 @@ function makeASpiral(n) {
     return result;
 }
 
-console.log(makeASpiral(5));
+console.log(makeASpiral(10));
 

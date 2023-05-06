80b01b4e843f3d83ddd161cf24b637416540b2e7
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-06 20:24:24 +0300

    feat: add task make a spiral

---
+++ make_a_spiral/test_4.js
@@ -0,0 +1,61 @@
+function makeASpiral(n) {
+    const matrix = new Array(n).fill().map(() => new Array(n).fill(0)); // создать пустую матрицу n*n
+
+    let rowStart = 0,
+        rowEnd = n - 1,
+        colStart = 0,
+        colEnd = n - 1,
+        num = 1,
+        val = 1;
+
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+        // top row
+        for (let i = colStart; i <= colEnd; i++) {
+            matrix[rowStart][i] = val;
+        }
+        rowStart++;
+        // right column
+        for (let i = rowStart; i <= rowEnd; i++) {
+            matrix[i][colEnd] = val;
+        }
+        colEnd--;
+
+        if (rowStart <= rowEnd) {
+            // bottom row
+            for (let i = colEnd; i >= colStart; i--) {
+                matrix[rowEnd][i] = val;
+            }
+            rowEnd--;
+        }
+
+        if (colStart <= colEnd) {
+            // left column
+            for (let i = rowEnd; i >= rowStart; i--) {
+                matrix[i][colStart] = val;
+            }
+            colStart++;
+        }
+
+        val = 1 - val;  // Переключение между 0 и 1 в каждом проходе по спирали
+    }
+
+    return matrix;
+}
+
+console.log(makeASpiral(5)); // [[1,1,1,1,1],[0,0,0,0,1],[1,1,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+
+console.log(makeASpiral(10));
+
+/*
+ [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]*/

c5adbd4209980db4d474a53278b474e94a7854d7
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-06 20:11:10 +0300

    feat: add task make a spiral

---
+++ make_a_spiral/spiralize.js
@@ -0,0 +1,74 @@
+function makeASpiral(n) {
+    const matrix = new Array(n).fill().map(() => new Array(n).fill(0)); // создать пустую матрицу n*n
+
+    let rowStart = 0,
+        rowEnd = n - 1,
+        colStart = 0,
+        colEnd = n - 1,
+        num = 1,
+        val = 1;
+
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+        // top row
+        for (let i = colStart; i <= colEnd; i++) {
+            if (i === 0 && rowStart === 1) {
+                matrix[rowStart][i] = 0;
+            } else {
+                matrix[rowStart][i] = val;
+            }
+        }
+        rowStart++;
+        // right column
+        for (let i = rowStart; i <= rowEnd; i++) {
+            if (i === 2 && colEnd === 1) {
+                matrix[i][colEnd] = 1;
+            } else {
+                matrix[i][colEnd] = val;
+            }
+        }
+        colEnd--;
+
+        // bottom row
+        for (let i = colEnd; i >= colStart; i--) {
+            if (i === colStart && rowEnd === 1) {
+                matrix[rowEnd][i] = 1;
+            } else {
+                matrix[rowEnd][i] = val;
+            }
+        }
+        rowEnd--;
+
+        // left column
+        for (let i = rowEnd; i >= rowStart; i--) {
+            if (i === 1 && colStart === 0) {
+                matrix[i][colStart] = 0;
+            } else {
+                matrix[i][colStart] = val;
+            }
+        }
+        colStart++;
+
+        val = 1 - val; // Переключение между 0 и 1 в каждом проходе по спирали
+    }
+
+    return matrix;
+}
+
+
+console.log(makeASpiral(10));
+
+console.log(makeASpiral(5)); // [[1,1,1,1,1],[0,0,0,0,1],[1,1,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+
+/*
+ [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]*/

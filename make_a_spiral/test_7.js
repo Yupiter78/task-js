92cf6604bedb975d4bf9c505292b3458fb445928
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-13 00:48:34 +0300

    feat: add task the make a spiral

--- make_a_spiral/test_7.js
+++ make_a_spiral/test_7.js
@@ -1,25 +1,3 @@
-/*const spiralize = (size) => {
-    const getIndex = (idx, size) => (idx < size / 2) ? idx : size - 1 - idx;
-    const result = Array.from({length: size}, () => new Array(size).fill(0));
-        result.map( (row, rowIdx) => {
-            row.map((col, colIdx) => {
-                console.log('rowIdx <= size / 2 - 1 + Math.sign(size % 4) && rowIdx - colIdx === 1 //',
-                     `${rowIdx} <= ${size} / 2 - 1 + ${Math.sign(size % 4)} && ${rowIdx} - ${colIdx} === 1 //`,
-                    rowIdx <= size / 2 - 1 + Math.sign(size % 4) && rowIdx - colIdx === 1);
-
-                console.log(`size / 2 - 1 + Math.sign(${size % 4})`, size / 2 - 1 + Math.sign(size % 4));
-                const shouldReverse = rowIdx <= size / 2 - 1 + Math.sign(size % 4) &&
-                    rowIdx - colIdx === 1;
-                const rowIdxMirror = getIndex(rowIdx, size),
-                    colIdxMirror = getIndex(colIdx, size);
-                return rowIdxMirror % 2 && rowIdxMirror <= colIdxMirror ||
-                colIdxMirror % 2 && rowIdxMirror >= colIdxMirror ?
-                    Number(shouldReverse) : Number(!shouldReverse);
-
-            })
-        });
-    return result;
-}*/
 
 const spiralize = (size) => {
     const getIndex = idx => (idx < size / 2) ? idx : size - 1 - idx,

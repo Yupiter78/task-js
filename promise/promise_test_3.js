b6342b9fcff16c49679336432722d9dc41f5636b
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-16 00:45:04 +0300

    feat: add task promise

--- promise/promise_test_3.js
+++ promise/promise_test_3.js
@@ -1,5 +1,5 @@
 
-const request = (url, success, error) => {
+/*const request = (url, success, error) => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.onload = () => {
@@ -14,4 +14,25 @@ const request = (url, success, error) => {
     }, error => {
       console.error(error);
     });
-  }
+  }*/
+
+const request = (url) => {
+
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        console.log(xhr);
+        xhr.open('GET', url);
+        xhr.onload = () => {
+            console.log(xhr.status);
+            xhr.status === 200 ? resolve(xhr.response) : reject(xhr.statusText);
+        }
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send();
+    })
+
+}
+document.querySelector('#get-text').onclick = () => {
+    request('https://restcountries.com/v3.1/all')
+        .then(data => document.querySelector('#result').innerHTML = data)
+        .catch(error => console.error(error));
+}

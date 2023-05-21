74f772d1b327e8dbea74ccc4f635c7b1e671da16
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-22 00:38:11 +0300

    feat: add task promise

--- promise/promise_test_7.js
+++ promise/promise_test_7.js
@@ -1,5 +1,5 @@
 
-// Create a function that makes an API request and returns a Promise
+/*// Create a function that makes an API request and returns a Promise
 function getData(url) {
     return new Promise((resolve, reject) => {
         fetch(url)
@@ -19,6 +19,12 @@ function getData(url) {
                 reject(error);
             });
     });
+}*/
+
+function getData(url) {
+    return fetch(url)
+            .then(response => response.ok ? response.json() : new Error(`Error ${response.status}`))
+            .catch(error =>  console.log("ErrOR", error));
 }
 
 // Use the function to request data
@@ -27,7 +33,7 @@ getData('https://jsonplaceholder.typicode.com/todos/1')
         console.log(data);
     })
     .catch(error => {
-        console.error(error);
+        console.log("ERROR", error);
     });
 
 

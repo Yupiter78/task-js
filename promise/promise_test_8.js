c76bf134fcaba7b7cff0fded71c832394260269c
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-23 23:55:27 +0300

    feat: add task promise

---
+++ promise/promise_test_8.js
@@ -0,0 +1,17 @@
+/*Create a function that takes an array of Promises and returns
+a Promise that resolves with an array of resolved values:*/
+
+function allSettled(promises) {
+    return Promise.all(promises.map(p => Promise.resolve(p).then(value => ({
+        status: "fulfilled",
+        value
+    }), reason => ({
+        status: "rejected",
+        reason
+    }))));
+}
+
+// usage example
+const promises = [Promise.resolve(1), Promise.resolve(2), Promise.reject("error")];
+allSettled(promises).then(results => console.log(results));
+// output: [{ status: "fulfilled", value: 1 }, { status: "fulfilled", value: 2 }, { status: "rejected", reason: "error" }]

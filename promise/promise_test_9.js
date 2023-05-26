9258d4ac6aef3ddebac79fbe1e01e6fbd3f0d121
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-26 23:59:29 +0300

    feat: add task promise

---
+++ promise/promise_test_9.js
@@ -0,0 +1,21 @@
+function getRequest(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(JSON.parse(xhr.responseText))
+            } else {
+                reject(new Error(`data loading error: ${xhr.responseText}`))
+            }
+        };
+        xhr.onerror = () => reject(new Error(`Network error`));
+        xhr.send();
+    })
+}
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+getRequest(url)
+    .then(data => console.log("Data uploaded successfully", data))
+    .catch(error => console.log("An error has occurred"));

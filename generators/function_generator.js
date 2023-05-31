31c83c4e63c564e25bebeebb0f225e9883f0b869
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-05-31 23:36:31 +0300

    feat: add task generators

---
+++ generators/function_generator.js
@@ -0,0 +1,12 @@
+function* generatorOne() {
+    yield ["a", "b", "c"];
+}
+function* generatorTwo() {
+    yield* ["a", "b", "c"];
+}
+
+const one = generatorOne();
+const two = generatorTwo();
+
+console.log(one.next().value);
+console.log(two.next().value);

578641b61f4bbc973226f176c802dcd6210c5ba7
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-06-01 23:42:18 +0300

    feat: add task promise

--- promise/promise_test_10.js
+++ promise/promise_test_10.js
@@ -1,4 +1,4 @@
-async function getDataSequentially(urls) {
+/*async function getDataSequentially(urls) {
     const results = [];
     for (const url of urls) {
         const response = await fetch(url);
@@ -13,6 +13,31 @@ getDataSequentially([
     "https://jsonplaceholder.typicode.com/posts/1",
     "https://jsonplaceholder.typicode.com/posts/2",
     "https://jsonplaceholder.typicode.com/posts/3"
-]).then(results => console.log(results));
+]).then(results => console.log(results));*/
+
+const urls = [
+    'https://api.github.com/users/iliakan',
+    'https://api.github.com/users/remy',
+    'https://api.github.com/users/jeresig'
+];
+
+const getResponses = async (urls) => {
+    try {
+        const responses = await Promise.all(urls.map(url => fetch(url)));
+        return  await Promise.all(responses.map(async (response) => {
+            const { name, location } = await response.json();
+            return { name, location };
+        }));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getResponses(urls)
+    .then(responseData => console.log({ responseData }))
+    .catch(error => console.log(error));
+
+
+
 
 
--- promise/promise_test_11.js
+++ promise/promise_test_11.js
@@ -9,3 +9,10 @@ getDataSimultaneously([
     "https://jsonplaceholder.typicode.com/posts/2",
     "https://jsonplaceholder.typicode.com/posts/3"
 ]).then(results => console.log(results));
+
+async function f() {return 1;}
+// console.log(f());
+f().then(console.log); // 1
+async function f2() {return Promise.resolve(1);}
+// console.log(f2());
+f2().then(console.log); // 1

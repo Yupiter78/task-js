2594c6cc2fca9501cb7716b33947582bab66ceb5
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-06-03 09:22:11 +0300

    feat: add task module axios

---
+++ module_axios/index.js
@@ -0,0 +1,9 @@
+
+    //using the methods of the global object axios
+    axios.get("https://jsonplaceholder.typicode.com/todos/1")
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });

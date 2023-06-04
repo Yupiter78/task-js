5964941f180bcd8e05ca7f2e3cf100228db70dba
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-06-04 22:43:11 +0300

    feat: add task promise

--- promise/fetch_data_from_api.js
+++ promise/fetch_data_from_api.js
@@ -3,8 +3,9 @@ function fetchDataFromAPI(endpointURL) {
     const cachedData = localStorage.getItem(endpointURL);
 
     if (cachedData !== null) {
+        console.log("localStorage");
         // Data is cached, return cached data
-        return JSON.parse(cachedData);
+        return Promise.resolve(JSON.parse(cachedData));
     } else {
         // Data is not cached, fetch from API
         return fetch(endpointURL)
@@ -21,5 +22,48 @@ function fetchDataFromAPI(endpointURL) {
 
  // Fetches posts data from JSONPlaceholder API and caches it locally
 const endpointURL = 'https://jsonplaceholder.typicode.com/posts';
-const postsData = fetchDataFromAPI(endpointURL);
-console.log(postsData); // Returns the cached or fetched posts data
+const postsData = fetchDataFromAPI(endpointURL).then(data => console.log(data));
+//console.log(postsData); // Returns the cached or fetched posts data
+
+
+ //Fetches users data from Reqres API and caches it locally
+
+const endpointURL_2 = 'https://reqres.in/api/users?page=2';
+const usersData = fetchDataFromAPI(endpointURL_2).then(data => console.log(data));
+//console.log(usersData); // Returns the cached or fetched users data for page 2
+
+
+ // Fetches users data from MockAPI API and caches it locally
+
+/*const endpointURL_3 = 'https://mockapi.io/api/v1/users';
+const usersData_2 = fetchDataFromAPI(endpointURL_3).then(data => console.log(data));*/
+//console.log(usersData_2); // Returns the cached or fetched users data
+
+
+
+// Fetches users data from Beeceptor API and caches it locally
+
+/*const endpointURL_4 = 'https://my-api.beeceptor.com/users';
+const usersData_3 = fetchDataFromAPI(endpointURL_4).then(data => console.log(data));
+console.log(usersData_3); // Returns the cached or fetched users data*/
+
+async function fetchDataWithCache(url) {
+    const cacheKey = "cache:" + url;
+    const cache = localStorage.getItem(cacheKey);
+
+    if (cache) {
+        console.log("localStorage");
+        return JSON.parse(cache);
+    } else {
+        const response = await fetch(url);
+        const data = await response.json();
+        localStorage.setItem(cacheKey, JSON.stringify(data));
+        return data;
+    }
+}
+
+// usage example
+fetchDataWithCache("https://jsonplaceholder.typicode.com/posts/1")
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+

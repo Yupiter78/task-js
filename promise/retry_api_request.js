4d7ee433c99649928aaf4938f2eb1e9d6f3c915f
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-06-09 23:57:05 +0300

    feat: add task promise

--- promise/retry_api_request.js
+++ promise/retry_api_request.js
@@ -2,9 +2,12 @@ async function retryApiRequest(path, retries = 3, delay = 1000) {
     const url = `https://jsonplaceholder.typicode.com/${path}`;
     for (let i = 0; i < retries; i++) {
         try {
+            console.log("first try");
             const response = await fetch(url);
+            console.log(response.status);
+            console.log(response.ok);
             if (!response.ok) {
-                throw new Error('Request failed');
+                throw new Error('_____Request failed______');
             }
             return await response.json();
         } catch (error) {
@@ -24,3 +27,36 @@ async function retryApiRequest(path, retries = 3, delay = 1000) {
         console.error(error);
     }
 })();
+
+// Example usage:
+(async () => {
+    try {
+        const posts = await retryApiRequest('posts/1000', 5, 2000);
+        console.log(posts);
+    } catch (error) {
+        console.error('Failed to fetch posts:', error);
+    }
+})();
+
+const multipleRequest = async (fn, limit) => {
+    let retries = 0;
+    while (retries < limit) {
+        try {
+            console.log("second try");
+            const response = await fn();
+            console.log(response.ok);
+            console.log(response.status);
+            if (!response.ok) throw new Error(`a request error occurred`);
+            return response.json();
+        } catch (e) {
+            console.log(e.message);
+            retries++;
+        }
+    }
+    throw new Error(`function failed after ${limit} retries`);
+}
+
+// usage example
+multipleRequest(() => fetch(`https://jsonplaceholder.tpicode.com/posts`), 3)
+    .then(data => console.log(data))
+    .catch(error => console.error(error.message));

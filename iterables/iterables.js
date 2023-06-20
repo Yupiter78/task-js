7420cdd53df8b13e7089beea2bc042bfc81d94b1
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-06-20 22:33:15 +0300

    feat: add task iterables

---
+++ iterables/iterables.js
@@ -0,0 +1,23 @@
+const range =  {
+    from: 1,
+    to: 5,
+    [Symbol.iterator] () {
+            return {
+                current: this.from,
+                last: this.to,
+                next () {
+                    return (this.current <= this.last) ?  {
+                            done: false,
+                            value: this.current++
+                        } : {
+                            done: true
+                        }
+                }
+            }
+    }
+}
+
+
+for (let num of range) {
+    console.log(num);
+}

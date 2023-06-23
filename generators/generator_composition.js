204e0fc6ef0cf2538656557d2fb087b83bd3075b
Yuriy Vedernikov <yuriy.jsx@gmail.com>
2023-06-23 10:05:57 +0300

    feat: add task generators

--- generators/generator_composition.js
+++ generators/generator_composition.js
@@ -5,6 +5,7 @@ function* generateSequence(start, end) {
 }
 
 const generator = generateSequence(4, 11);
+console.log("generator:", generator);
 
 /*
 for (let value of generator) {
@@ -25,3 +26,17 @@ for (let code of generatePasswordCode()) {
 }
 
 console.log("str:", str);
+function* generatePasswordCode_2() {
+    for (let i = 48; i <= 57; i++) yield i;
+    for (let i = 65; i <= 90; i++) yield i;
+    for (let i = 97; i <= 122; i++) yield i;
+}
+
+let str_2 = "";
+for (let code of generatePasswordCode_2()) {
+    str_2 += String.fromCodePoint(code);
+}
+
+console.log("str_2:", str_2);
+
+console.log("str === str_2:", str === str_2);
